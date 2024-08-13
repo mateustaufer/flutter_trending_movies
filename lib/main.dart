82de3fa891acabadce378a74fde0dbfdc8138f21
Mateus Taufer <mateustaufer@gmail.com>
2024-08-13 16:27:25 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'trending_movie_app.dart';
+
+void main() {
+  runApp(const TrendingMovieApp());
+}
---
+++ lib/trending_movie_app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'counter_page_view.dart';
+
+class TrendingMovieApp extends StatelessWidget {
+  const TrendingMovieApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Trending Movies',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const CounterPageView(title: 'Counter'),
+    );
+  }
+}
