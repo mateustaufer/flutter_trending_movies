573f9d27337218a0922929409a19ae2ede6d1e58
Mateus Taufer <mateustaufer@gmail.com>
2024-08-14 20:14:07 -0300

    add some tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_trending_movies/trending_movie_app.dart';
+import 'package:flutter_trending_movies/data/network/network_response.dart';
+import 'package:flutter_trending_movies/data/repositories/movie_repository.dart';
+import 'package:flutter_trending_movies/views/counter_page_view.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'mocks/movie_mock.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const TrendingMovieApp());
+    await tester.pumpWidget(
+      const MaterialApp(home: CounterPageView(title: 'Counter')),
+    );
 
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
@@ -14,5 +21,26 @@ void main() {
 
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+  });
+
+  final movieMock = MovieMock();
+  final movieRepository = MovieRepository(movieMock);
+
+  test('Fetch Trending Movies List', () async {
+    when(
+      () => movieMock.fetchTrendingMoviesList(timeWindow: 'day'),
+    ).thenAnswer(
+      (_) async =>
+          NetworkResponse(body: MovieMock.responseBody, statusCode: 200),
+    );
+
+    final response = await movieRepository.fetchTrendingMoviesList(
+      timeWindow: 'day',
+    );
+
+    response.fold(
+      (l) => expect(l.statusMessage, isNotEmpty),
+      (r) => expect(r.movies, isNotEmpty),
+    );
   });
 }
