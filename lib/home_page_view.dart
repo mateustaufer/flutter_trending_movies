36f737eb5a90b532d16a5003c3f9a160340a87a0
Mateus Taufer <mateustaufer@gmail.com>
2024-08-14 17:00:53 -0300

    fixing merge problems with development

--- lib/home_page_view.dart
+++ lib/home_page_view.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'data/models/movies_list_model.dart';
-import 'data/providers/movie_provider.dart';
-import 'data/repositories/movie_repository.dart';
-import 'widgets/base_page_widget.dart';
+import '../controllers/home_page_controller.dart';
+import '../data/models/movie_model.dart';
+import '../widgets/base_page_widget.dart';
 
 class HomePageView extends StatefulWidget {
   const HomePageView({super.key});
@@ -13,35 +12,46 @@ class HomePageView extends StatefulWidget {
 }
 
 class _HomePageViewState extends State<HomePageView> {
-  final moviesRepository = MovieRepository(MovieProvider());
-  MoviesListModel moviesList = MoviesListModel();
+  final controller = HomePageController();
 
   @override
   void initState() {
     super.initState();
 
-    moviesRepository.fetchMoviesList(listId: 1).then((value) {
-      setState(() {
-        moviesList = value;
-      });
-    });
+    controller.fetchTrendingMovies(context);
   }
 
   @override
   Widget build(BuildContext context) {
     return BasePageWidget(
-      appBar: AppBar(title: const Text('Movies List')),
-      body: ListView.separated(
-        itemCount: moviesList.movies?.length ?? 0,
-        itemBuilder: (_, index) => ListTile(
-            title: Text(moviesList.movies?[index].title ?? 'deu ruim')),
-        separatorBuilder: (_, index) {
-          if (index < (moviesList.movies?.length ?? 0)) {
-            return const SizedBox(height: 8);
-          }
-
-          return const SizedBox.shrink();
+      appBar: AppBar(title: const Text('Trending Movies')),
+      body: ValueListenableBuilder<bool>(
+        valueListenable: controller.isLoading,
+        builder: (context, value, child) {
+          return value
+              ? const Center(child: CircularProgressIndicator())
+              : child ?? const SizedBox.shrink();
         },
+        child: ValueListenableBuilder<List<MovieModel>>(
+          valueListenable: controller.movies,
+          builder: (context, value, child) {
+            return value.isEmpty
+                ? const Center(child: Text('A lista de filmes está vazia!'))
+                : ListView.separated(
+                    itemCount: value.length,
+                    itemBuilder: (_, index) => ListTile(
+                      title: Text(value[index].title ?? ''),
+                    ),
+                    separatorBuilder: (_, index) {
+                      if (index < (value.length)) {
+                        return const SizedBox(height: 8);
+                      }
+
+                      return const SizedBox.shrink();
+                    },
+                  );
+          },
+        ),
       ),
     );
   }
