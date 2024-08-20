0b3db3d991f43de212c6b8cd095c46b37cb10a72
Mateus Taufer <mateustaufer@gmail.com>
2024-08-20 12:10:32 -0300

    remove package url_strategy and add a splash screen in project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:url_strategy/url_strategy.dart';
 
+import 'core/configure_nonweb.dart'
+    if (dart.library.html) 'core/configure_web.dart';
 import 'core/dependencies_injecttion.dart';
 import 'trending_movie_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  setPathUrlStrategy();
+  configureUrlStrategy();
 
   DependenciesInjection.init();
 
