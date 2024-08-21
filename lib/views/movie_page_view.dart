5983a760e6546f658656ca739f7347781f9d2f7f
Mateus Taufer <mateustaufer@gmail.com>
2024-08-21 18:18:34 -0300

    add a title in app bar

--- lib/views/movie_page_view.dart
+++ lib/views/movie_page_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shimmer/shimmer.dart';
 
+import '../data/states/movie_state.dart';
 import '../data/stores/movie_store.dart';
 import '../widgets/base_page_widget.dart';
 
@@ -16,21 +18,67 @@ class MoviePageView extends StatefulWidget {
 class _MoviePageViewState extends State<MoviePageView> {
   final movieStore = GetIt.I.get<MovieStore>();
 
+  final isLoading = ValueNotifier<bool>(true);
+  late final String title;
+
   @override
   void initState() {
     super.initState();
 
-    movieStore.fetchMovieDetails(movieId: widget.id);
+    movieStore.fetchMovieDetails(movieId: widget.id).whenComplete(() {
+      if (movieStore.value is MovieSuccessState) {
+        title = (movieStore.value as MovieSuccessState).movie.title ?? '';
+      } else {
+        title = 'Erro ao carregar o filme';
+      }
+
+      isLoading.value = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return BasePageWidget(
       appBar: AppBar(
-        title: const Text('Movie Details'),
+        title: ValueListenableBuilder(
+            valueListenable: isLoading,
+            builder: (context, value, child) {
+              if (value) {
+                return Shimmer.fromColors(
+                  baseColor: Colors.grey.shade100,
+                  highlightColor: Colors.grey.shade300,
+                  child: Container(
+                    width: 300,
+                    height: 48,
+                    color: Colors.white,
+                  ),
+                );
+              }
+
+              return Text(title);
+            }),
       ),
-      body: Center(
-        child: Text('Movie ID: ${widget.id}'),
+      body: ValueListenableBuilder(
+        valueListenable: movieStore,
+        builder: (context, state, child) {
+          if (state is MovieLoadingState) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (state is MovieErrorState) {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(state.message), showCloseIcon: true),
+              );
+            });
+          }
+
+          if (state is MovieSuccessState) {
+            return Center(child: Text('Id do filme: ${state.movie.id}'));
+          }
+
+          return const Center(child: Text('A lista de filmes está vazia!'));
+        },
       ),
     );
   }
