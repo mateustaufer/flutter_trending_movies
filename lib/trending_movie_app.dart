c9fddfa31a36b2b2944a2f5afe8765110d05ffc2
Mateus Taufer <mateustaufer@gmail.com>
2024-08-21 03:49:40 -0300

    including go router again

--- lib/trending_movie_app.dart
+++ lib/trending_movie_app.dart
@@ -7,19 +7,14 @@ class TrendingMovieApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Trending Movies',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      initialRoute: Routes.home,
-      routes: Routes.instance.routes,
-      onGenerateRoute: (settings) {
-        print('Route: ${settings.name}');
-        return null;
-      },
+      routerConfig: Routes.instance.routerConfig,
     );
   }
 }
