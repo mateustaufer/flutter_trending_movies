cab2d13161e11f1a108591572dcb0dcf0f11d462
Mateus Taufer <mateustaufer@gmail.com>
2024-08-23 14:14:07 -0300

    fix configure_web file path in main

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'core/bindings/bindings.dart';
 import 'trending_movie_app.dart';
 import 'utils/configure_nonweb.dart'
-    if (dart.library.html) 'core/configure_web.dart';
+    if (dart.library.html) 'utils/configure_web.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
